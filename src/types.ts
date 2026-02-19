b7630b0c7ed3451f9af58d935abe0d9e1821a5f3
Dennis Quilatan <dnnsqltn@yahoo.com>
2026-02-19 18:08:05 +0800

    implement Home and Products pages with components

---
+++ src/types.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export type ProductType = {
+  id: string | number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: Record<string, string>;
+};
+
+export type ProductsType = ProductType[];
+
+export type CartItemType = ProductType & {
+  quantity: number;
+  selectedSize: string;
+  selectedColor: string;
+};
+
+export type CartItemsType = CartItemType[];
+
+export const ShippingFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email().min(1, "Email is required"),
+  phone: z
+    .string()
+    .min(7, "Phone number must be between 7 and 10 digits")
+    .max(10, "Phone number must be between 7 and 10 digits")
+    .regex(/^\d+$/, "Phone number must contain only numbers"),
+  address: z.string().min(1, "Address is required"),
+  city: z.string().min(1, "City is required"),
+  // postalCode: z.string().min(1, "Postal code is required"),
+  // country: z.string().min(1, "Country is required"),
+});
+
+export type ShippingFormInputs = z.infer<typeof ShippingFormSchema>;
